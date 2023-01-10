beccc44b9e8909d48bd79c0b078f4b9d9eb7fa79
glasket <cws@glasket.com>
2023-01-09 23:42:38 -0500

    Add JSON support to Set

--- set/set_test.go
+++ set/set_test.go
@@ -8,6 +8,7 @@
 package set_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	. "github.com/glasket/datastructures/set"
@@ -16,7 +17,20 @@ import (
 var set Set[int]
 
 func init() {
-	set = *NewSet[int]()
+	set = NewSet[int](0)
+}
+
+func TestNewSetFromSlice(t *testing.T) {
+	vals := []int{1, 2, 3, 4}
+	s := NewSetFromSlice(vals)
+	if s.Count() != len(vals) {
+		t.Errorf("expected %v items, found %v", len(vals), s.Count())
+	}
+	for _, v := range vals {
+		if !s.Contains(v) {
+			t.Errorf("%v not in set", v)
+		}
+	}
 }
 
 func TestInsert(t *testing.T) {
@@ -29,6 +43,29 @@ func TestInsert(t *testing.T) {
 	}
 	if err := set.Insert(1); err == nil {
 		t.Error("set failed to catch that value is already present before insertion")
+	}
+}
+
+func TestJSON(t *testing.T) {
+	j, err := json.Marshal(set)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(j) != "[1,2,3,4]" {
+		t.Error("json marshalled incorrectly")
+	}
+
+	var s Set[int]
+	err = json.Unmarshal(j, &s)
+	if err != nil {
+		t.Error(err)
+	}
+	missing := findValues(s.Values(), set.Values())
+	if len(missing) != 0 {
+		t.Error("json unmarshalled incorrectly")
+		for _, v := range missing {
+			t.Errorf("%v missing", v)
+		}
 	}
 }
 
@@ -47,16 +84,10 @@ func TestValues(t *testing.T) {
 	if len(set.Values()) != len(expect) {
 		t.Errorf("Not enough values. %v/%v", len(set.Values()), len(expect))
 	}
-	var found bool
-	for _, v := range set.Values() {
-		found = false
-		for _, e := range expect {
-			if v == e {
-				found = true
-			}
-		}
-		if !found {
-			t.Errorf("Failed to find %v", v)
+	missing := findValues(set.Values(), expect)
+	if len(missing) != 0 {
+		for _, v := range missing {
+			t.Errorf("%v missing", v)
 		}
 	}
 }
@@ -69,4 +100,21 @@ func TestClear(t *testing.T) {
 			t.Log(v)
 		}
 	}
+}
+
+func findValues[V comparable](setVals []V, expectedVals []V) []V {
+	missing := []V{}
+	found := false
+	for _, v := range setVals {
+		found = false
+		for _, e := range expectedVals {
+			if v == e {
+				found = true
+			}
+		}
+		if !found {
+			missing = append(missing, v)
+		}
+	}
+	return missing
 }
