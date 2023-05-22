ccf0b58e81011dc808259ee95080a142e2ebdbfe
glasket <cws@glasket.com>
2023-05-22 02:31:12 -0400

    Rewrite Set

--- set/set.go
+++ set/set.go
@@ -14,14 +14,10 @@ import (
 
 var fil struct{} = struct{}{}
 
-type Set[V comparable] struct {
-	values map[V]struct{}
-}
+type Set[V comparable] map[V]struct{}
 
 func NewSet[V comparable](size int) Set[V] {
-	return Set[V]{
-		values: make(map[V]struct{}, size),
-	}
+	return make(map[V]struct{}, size)
 }
 
 // Creates a set from a preexisting slice
@@ -30,9 +26,7 @@ func NewSet[V comparable](size int) Set[V] {
 // If you want to catch errors, either use NewSet and manually Insert,
 // or check the size of the slice against the resulting set.
 func NewSetFromSlice[V comparable](slice []V) Set[V] {
-	set := Set[V]{
-		values: make(map[V]struct{}, len(slice)),
-	}
+	set := NewSet[V](len(slice))
 	for _, v := range slice {
 		set.Insert(v) // We don't care if it errors
 	}
@@ -42,48 +36,151 @@ func NewSetFromSlice[V comparable](slice []V) Set[V] {
 // Inserts a given value into the set.
 //
 // Throws an error if the value is already present.
-func (s *Set[V]) Insert(value V) error {
+func (s Set[V]) Insert(value V) error {
 	if s.Contains(value) {
 		return fmt.Errorf("value %v is already present", value)
 	}
-	s.values[value] = fil
+	s[value] = fil
 	return nil
 }
 
-func (s *Set[V]) Remove(value V) error {
+// Removes the given value from the set.
+//
+// Throws an error if the value is not present.
+func (s Set[V]) Remove(value V) error {
 	if !s.Contains(value) {
 		return fmt.Errorf("value %v is not present", value)
 	}
-	delete(s.values, value)
+	delete(s, value)
 	return nil
 }
 
-func (s *Set[V]) Clear() {
-	for v := range s.values {
-		delete(s.values, v)
+// Deletes all values from the set.
+func (s Set[V]) Clear() {
+	for v := range s {
+		delete(s, v)
 	}
 }
 
+// Returns true if the given value is present in the set.
 func (s Set[V]) Contains(value V) bool {
-	_, ok := s.values[value]
+	_, ok := s[value]
 	return ok
 }
 
+// Returns the number of values in the set.
 func (s Set[V]) Count() int {
-	return len(s.values)
+	return len(s)
 }
 
+// Returns a slice of all values in the set.
 func (s Set[V]) Values() []V {
-	values := make([]V, len(s.values))
+	values := make([]V, len(s))
 	i := 0
 
-	for v := range s.values {
+	for v := range s {
 		values[i] = v
 		i++
 	}
 
 	return values
 }
+
+// Test if two sets are equal.
+//
+// Two sets are equal if they contain the same values.
+func (s Set[V]) Equals(other Set[V]) bool {
+	if s.Count() != other.Count() {
+		return false
+	}
+
+	for v := range s {
+		if !other.Contains(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Returns a new set containing the values in both sets.
+//
+// Does not modify either set.
+func (s Set[V]) Union(other Set[V]) Set[V] {
+	union := NewSet[V](s.Count() + other.Count())
+
+	for v := range s {
+		union.Insert(v)
+	}
+	for v := range other {
+		union.Insert(v)
+	}
+
+	return union
+}
+
+// Returns a new set containing the values in both sets.
+//
+// Does not modify either set.
+func (s Set[V]) Intersection(other Set[V]) Set[V] {
+	intersection := NewSet[V](s.Count())
+
+	for v := range s {
+		if other.Contains(v) {
+			intersection.Insert(v)
+		}
+	}
+
+	return intersection
+}
+
+// Returns a new set containing the values in the first set but not the second.
+//
+// Does not modify either set.
+func (s Set[V]) Difference(other Set[V]) Set[V] {
+	difference := NewSet[V](s.Count())
+
+	for v := range s {
+		if !other.Contains(v) {
+			difference.Insert(v)
+		}
+	}
+
+	return difference
+}
+
+// Returns a new set containing the values in exactly one of the sets.
+//
+// Does not modify either set.
+func (s Set[V]) SymmetricDifference(other Set[V]) Set[V] {
+	union := s.Union(other)
+	intersection := s.Intersection(other)
+
+	return union.Difference(intersection)
+}
+
+// Returns true if the first set is a subset of the second.
+func (s Set[V]) SubsetOf(other Set[V]) bool {
+	if s.Count() > other.Count() {
+		return false
+	}
+
+	for v := range s {
+		if !other.Contains(v) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Returns true if the first set is a superset of the second.
+func (s Set[V]) SupersetOf(other Set[V]) bool {
+	return other.SubsetOf(s)
+}
+
+// Returns a JSON array of the set's values.
+//
+// Uses Set.Values.
 
 func (s Set[V]) MarshalJSON() ([]byte, error) {
 	return json.Marshal(s.Values())
--- set/set_test.go
+++ set/set_test.go
@@ -9,112 +9,238 @@ package set_test
 
 import (
 	"encoding/json"
+	"sort"
 	"testing"
 
 	. "github.com/glasket/datastructures/set"
 )
 
-var set Set[int]
-
-func init() {
-	set = NewSet[int](0)
+func TestNewSet(t *testing.T) {
+	set := NewSet[int](5)
+	if len(set) != 0 {
+		t.Fatalf("Expected NewSet to return a set of size 0, got %d", len(set))
+	}
 }
 
 func TestNewSetFromSlice(t *testing.T) {
-	vals := []int{1, 2, 3, 4}
-	s := NewSetFromSlice(vals)
-	if s.Count() != len(vals) {
-		t.Errorf("expected %v items, found %v", len(vals), s.Count())
+	set := NewSetFromSlice([]int{1, 2, 3, 4, 5})
+	if len(set) != 5 {
+		t.Fatalf("Expected NewSetFromSlice to return a set of size 5, got %d", len(set))
 	}
-	for _, v := range vals {
-		if !s.Contains(v) {
-			t.Errorf("%v not in set", v)
+	for i := 1; i <= 5; i++ {
+		if !set.Contains(i) {
+			t.Errorf("Expected NewSetFromSlice to contain %d", i)
 		}
 	}
 }
 
-func TestInsert(t *testing.T) {
-	vals := []int{1, 2, 3, 4}
-	for _, v := range vals {
-		set.Insert(v)
-		if !set.Contains(v) {
-			t.Errorf("%v was not inserted", v)
-		}
-	}
-	if err := set.Insert(1); err == nil {
-		t.Error("set failed to catch that value is already present before insertion")
-	}
-}
-
-func TestJSON(t *testing.T) {
-	j, err := json.Marshal(set)
+func TestSetInsert(t *testing.T) {
+	set := NewSet[int](0)
+	err := set.Insert(5)
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Expected Insert to not error, got %v", err)
 	}
-	if string(j) != "[1,2,3,4]" {
-		t.Error("json marshalled incorrectly")
+	if len(set) != 1 {
+		t.Errorf("Expected Insert to insert a value, got %d", len(set))
 	}
+}
 
-	var s Set[int]
-	err = json.Unmarshal(j, &s)
+func TestSetInsertDuplicate(t *testing.T) {
+	set := NewSet[int](0)
+	err := set.Insert(5)
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Expected Insert to not error, got %v", err)
 	}
-	missing := findValues(s.Values(), set.Values())
-	if len(missing) != 0 {
-		t.Error("json unmarshalled incorrectly")
-		for _, v := range missing {
-			t.Errorf("%v missing", v)
-		}
+	err = set.Insert(5)
+	if err == nil {
+		t.Errorf("Expected Insert to error on duplicate, got nil")
 	}
-}
-
-func TestRemove(t *testing.T) {
-	set.Remove(1)
-	if set.Contains(1) {
-		t.Error("remove failed to remove 1")
-	}
-	if err := set.Remove(1); err == nil {
-		t.Error("set failed to catch that value wasn't present before removal")
+	if len(set) != 1 {
+		t.Errorf("Expected Insert to not insert a duplicate value, got %d", len(set))
 	}
 }
 
-func TestValues(t *testing.T) {
-	expect := []int{2, 3, 4}
-	if len(set.Values()) != len(expect) {
-		t.Errorf("Not enough values. %v/%v", len(set.Values()), len(expect))
+func TestSetRemove(t *testing.T) {
+	set := NewSet[int](0)
+	set.Insert(5)
+	err := set.Remove(5)
+	if err != nil {
+		t.Errorf("Expected Remove to not error, got %v", err)
 	}
-	missing := findValues(set.Values(), expect)
-	if len(missing) != 0 {
-		for _, v := range missing {
-			t.Errorf("%v missing", v)
-		}
+	if len(set) != 0 {
+		t.Errorf("Expected Remove to remove a value, got %d", len(set))
 	}
 }
 
-func TestClear(t *testing.T) {
+func TestSetRemoveMissing(t *testing.T) {
+	set := NewSet[int](0)
+	err := set.Remove(5)
+	if err == nil {
+		t.Errorf("Expected Remove to error on missing value, got nil")
+	}
+	if len(set) != 0 {
+		t.Errorf("Expected Remove to not remove a missing value, got %d", len(set))
+	}
+}
+
+func TestSetClear(t *testing.T) {
+	set := NewSet[int](0)
+	set.Insert(5)
 	set.Clear()
-	if set.Count() != 0 {
-		t.Errorf("set should be empty, found %v elements", set.Count())
-		for _, v := range set.Values() {
-			t.Log(v)
-		}
+	if len(set) != 0 {
+		t.Errorf("Expected Clear to clear set, got %d", len(set))
 	}
 }
 
-func findValues[V comparable](setVals []V, expectedVals []V) []V {
-	missing := []V{}
-	found := false
-	for _, v := range setVals {
-		found = false
-		for _, e := range expectedVals {
-			if v == e {
-				found = true
-			}
-		}
-		if !found {
-			missing = append(missing, v)
-		}
+func TestSetContains(t *testing.T) {
+	set := NewSet[int](0)
+	set.Insert(5)
+	if !set.Contains(5) {
+		t.Errorf("Expected Contains to return true, got false")
 	}
-	return missing
+	if set.Contains(6) {
+		t.Errorf("Expected Contains to return false, got true")
+	}
+}
+
+func TestSetCount(t *testing.T) {
+	set := NewSet[int](0)
+	set.Insert(5)
+	if set.Count() != 1 {
+		t.Errorf("Expected Count to return 1, got %d", set.Count())
+	}
+}
+
+func TestSetValues(t *testing.T) {
+	set := NewSet[int](0)
+	set.Insert(5)
+	set.Insert(6)
+	values := set.Values()
+	if len(values) != 2 {
+		t.Errorf("Expected Values to return a slice of size 2, got size %d", len(values))
+	}
+	sort.Slice(values, func(i, j int) bool { return values[i] < values[j] })
+	if values[0] != 5 || values[1] != 6 {
+		t.Errorf("Expected Values to return [5 6], got %v", values)
+	}
+}
+
+func TestSetEquals(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+	set2 := NewSetFromSlice([]int{1, 2, 3})
+
+	if !set1.Equals(set2) {
+		t.Errorf("Expected Equals to return true, got false")
+	}
+
+	set2.Insert(4)
+
+	if set1.Equals(set2) {
+		t.Errorf("Expected Equals to return false, got true")
+	}
+}
+
+func TestSetUnion(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+
+	set2 := NewSetFromSlice([]int{3, 4, 5})
+
+	union := set1.Union(set2)
+	if len(union) != 5 {
+		t.Errorf("Expected Union to return a set of size 5, got %d", len(union))
+	}
+	expected := NewSetFromSlice([]int{1, 2, 3, 4, 5})
+	if !union.Equals(expected) {
+		t.Errorf("Expected Union to return %v, got %v", expected, union)
+	}
+}
+
+func TestSetIntersection(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+
+	set2 := NewSetFromSlice([]int{3, 4, 5})
+
+	intersection := set1.Intersection(set2)
+	if len(intersection) != 1 {
+		t.Errorf("Expected Intersection to return a set of size 1, got %d", len(intersection))
+	}
+	expected := NewSetFromSlice([]int{3})
+	if !intersection.Equals(expected) {
+		t.Errorf("Expected Intersection to return %v, got %v", expected, intersection)
+	}
+}
+
+func TestSetDifference(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+
+	set2 := NewSetFromSlice([]int{3, 4, 5})
+
+	difference := set1.Difference(set2)
+	if len(difference) != 2 {
+		t.Errorf("Expected Difference to return a set of size 2, got %d", len(difference))
+	}
+	expected := NewSetFromSlice([]int{1, 2})
+	if !difference.Equals(expected) {
+		t.Errorf("Expected Difference to return %v, got %v", expected, difference)
+	}
+}
+
+func TestSetSymmetricDifference(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+
+	set2 := NewSetFromSlice([]int{3, 4, 5})
+
+	difference := set1.SymmetricDifference(set2)
+	if len(difference) != 4 {
+		t.Errorf("Expected SymmetricDifference to return a set of size 4, got %d", len(difference))
+	}
+	expected := NewSetFromSlice([]int{1, 2, 4, 5})
+	if !difference.Equals(expected) {
+		t.Errorf("Expected SymmetricDifference to return %v, got %v", expected, difference)
+	}
+}
+
+func TestSetSubsetOf(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+
+	set2 := NewSetFromSlice([]int{1, 2, 3, 4, 5})
+
+	if !set1.SubsetOf(set2) {
+		t.Errorf("Expected SubsetOf to return true, got false")
+	}
+	if set2.SubsetOf(set1) {
+		t.Errorf("Expected SubsetOf to return false, got true")
+	}
+}
+
+func TestSetSupersetOf(t *testing.T) {
+	set1 := NewSetFromSlice([]int{1, 2, 3})
+
+	set2 := NewSetFromSlice([]int{1, 2, 3, 4, 5})
+
+	if set1.SupersetOf(set2) {
+		t.Errorf("Expected SupersetOf to return false, got true")
+	}
+	if !set2.SupersetOf(set1) {
+		t.Errorf("Expected SupersetOf to return true, got false")
+	}
+}
+
+func TestSetJson(t *testing.T) {
+	set := NewSetFromSlice([]int{1, 2, 3, 4, 5})
+
+	jsonBytes, err := json.Marshal(set)
+	if err != nil {
+		t.Errorf("Expected Marshal to not error, got %v", err)
+	}
+
+	var set2 Set[int]
+	err = json.Unmarshal(jsonBytes, &set2)
+	if err != nil {
+		t.Errorf("Expected Unmarshal to not error, got %v", err)
+	}
+
+	if !set.Equals(set2) {
+		t.Errorf("Expected Unmarshal to return %v, got %v", set, set2)
+	}
 }
