ffd6327ef6769cf4d1a18e827d835e9f0c0a5e04
glasket <cws@glasket.com>
2023-10-29 00:28:54 -0400

    Fix license comment placement
    - Remove main.go

--- interfaces/iterator/iterator.go
+++ interfaces/iterator/iterator.go
@@ -5,6 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
 package iterator
 
 // Iterable provides a way to iterate over an implementors values using range.
